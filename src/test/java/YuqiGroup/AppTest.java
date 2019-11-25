c4379e038010164073eaba52e30bcd83c5b3fddd
oneaqi <50888398+oneaqi@users.noreply.github.com>
2019-11-25 13:56:36 +0100

    Update AppTest.java

--- src/test/java/YuqiGroup/AppTest.java
+++ src/test/java/YuqiGroup/AppTest.java
@@ -15,6 +15,23 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
+        String baseUrl = "http://demo.guru99.com/test/newtours/";
+        String pathToFirefoxDriver = "/usr/local/bin/geckodriver";
+        System.setProperty("webdriver.firefox.marionette", pathToFirefoxDriver);
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get(baseUrl);
+        String innerText = driver.findElement(By
+                .xpath("//table/tbody/tr/td[2]"
+                        + "//table/tbody/tr[4]/td/"
+                        + "table/tbody/tr/td[2]/"
+                        + "table/tbody/tr[2]/td[1]/"
+                        + "table[2]/tbody/tr[3]/td[2]/font"))
+                .getText();
+
+        System.out.println(innerText);
+       // driver.quit();
+        System.out.println( "Hello World!" );      
         assertTrue( true );
     }
 }
