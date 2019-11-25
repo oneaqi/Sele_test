c35f1e2057c4d16a12c5bc5cdc137ea6c113b4e6
oneaqi <50888398+oneaqi@users.noreply.github.com>
2019-11-25 14:10:30 +0100

    Update AppTest.java

--- src/test/java/YuqiGroup/AppTest.java
+++ src/test/java/YuqiGroup/AppTest.java
@@ -1,40 +1,69 @@
-package YuqiGroup;
-import org.openqa.selenium.By;
-import org.openqa.selenium.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import static org.junit.Assert.assertTrue;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        String baseUrl = "http://demo.guru99.com/test/newtours/";
-        String pathToFirefoxDriver = "/usr/local/bin/geckodriver";
-        System.setProperty("webdriver.firefox.marionette", pathToFirefoxDriver);
-        WebDriver driver = new FirefoxDriver();
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 
-        driver.get(baseUrl);
-        String innerText = driver.findElement(By
-                .xpath("//table/tbody/tr/td[2]"
-                        + "//table/tbody/tr[4]/td/"
-                        + "table/tbody/tr/td[2]/"
-                        + "table/tbody/tr[2]/td[1]/"
-                        + "table[2]/tbody/tr[3]/td[2]/font"))
-                .getText();
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-        System.out.println(innerText);
-       // driver.quit();
-        System.out.println( "Hello World!" );      
-        assertTrue( true );
+public class TestClass{
+    private WebDriver driver, driver2;
+    private WebDriverWait myWaitVar;
+    @Before
+
+    public void setup(){
+
+        String pathToChromeDriver = "C:\\Selenium\\chromedriver.exe";
+
+        System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        myWaitVar = new WebDriverWait(driver,10);
+
+
+
     }
+
+    @After
+    public void teardown() {
+
+        //driver.close();
+    }
+
+    @Test
+    public void loginTest () throws InterruptedException {
+
+        driver.navigate().to("http://192.168.56.103:8080");
+
+        WebElement username = driver.findElement(By.id("j_username"));
+
+        username.sendKeys("wrongName");
+        username.clear();
+        username.sendKeys("yzhang");
+
+        driver.findElement(By.name("j_password")).sendKeys("Notim@1234");
+        Thread.sleep(5000);
+        WebElement toSubmit = myWaitVar.until(ExpectedConditions.elementToBeClickable(By.className("submit")));
+        toSubmit.click();
+
+        String tagName = "";
+        tagName = driver.findElement(By.id("job_SimpleJob")).getTagName();
+        System.out.println(tagName);
+
+        driver.findElement(By.linkText("Manage Jenkins")).click();
+
+
+
+    }
+
+
 }
